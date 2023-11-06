885e5df7750a3b0422d9860fbf7ab79f3a0372c2
Aliec-AQ <amaury.quilliec@icloud.com>
2023-11-06 14:57:04 +0100

    ajout vendor via composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'touiteur/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7e854b4bbd02887f733f07b4feb4330ae61248ba',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'touiteur/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7e854b4bbd02887f733f07b4feb4330ae61248ba',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
