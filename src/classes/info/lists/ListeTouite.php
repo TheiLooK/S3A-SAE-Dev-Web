4c5caad27d1813358ed2cd0159265e38ddd4fd18
Aliec-AQ <amaury.quilliec@icloud.com>
2023-11-06 17:05:47 +0100

    creation des listes et les listes via tag

---
+++ src/classes/info/lists/ListeTouite.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+namespace touiteur\app\info\lists;
+
+class ListeTouite extends Liste {
+    public function __construct(){
+        parent::__construct();
+    }
+
+    /**
+     * Method to get the touites from the database and insert them into the list
+     * @return void
+     */
+    public function getListeTouite(){
+        //todo
+    }
+}
