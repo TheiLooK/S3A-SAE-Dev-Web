9b900274d645d0622884420a6f0f8da4bf7e8799
Z0ROZ <Fabioreveillard@gmail.com>
2023-11-06 16:16:41 +0100

    class abstract Action et structure de la class Dispatcher

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -2,7 +2,52 @@
 declare(strict_types=1);
 namespace touiteur\app\dispatch;
 
-class dispatcher
-{
+use touiteur\app\action\Action;
+class Dispatcher {
+
+
+    private ?string $action;
+
+    public function run(): void {
+        switch ($this->action) {
+            case '1':
+                $result = "a définir ";
+                break;
+            case '2':
+                $result = " a définir ";
+                break;
+            default :
+                $result = "a définir ";
+        }
+        $this->renderPage($result);
+    }
+
+    private function renderPage(string $html): void {
+        echo <<<END
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <title>Touiteur</title>
+    </head>
+    <body>
+    <header>
+        <nav>
+            <div> 
+                <h1>Deefy</h1>
+            </div>
+            <ul>
+            </ul>
+        </nav>
+    </header>
+    <div></div>
+        <div>
+            $html
+        </div>
+
+    </body>
+    </html>
+    END;
+    }
+
 
 }
