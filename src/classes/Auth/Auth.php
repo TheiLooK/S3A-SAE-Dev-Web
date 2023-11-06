1b7132c97ed0680496cd362b880c16056cee4c4f
Aliec-AQ <amaury.quilliec@icloud.com>
2023-11-06 16:04:25 +0100

    modification fichiers

--- src/classes/Auth/Auth.php
+++ src/classes/Auth/Auth.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types=1);
 namespace touiteur\app\Auth;
 
 class Auth
--- src/classes/Exception/AuthException.php
+++ src/classes/Exception/AuthException.php
@@ -2,7 +2,4 @@
 
 namespace touiteur\app\Exception;
 use Exception;
-class AuthException extends \Exception
-{
-
-}
+class AuthException extends \Exception{}
