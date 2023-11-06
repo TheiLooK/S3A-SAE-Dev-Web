c8558bee36a1361159de4a47aac08873e776abc3
Maxence C <maxchaix54@gmail.com>
2023-11-06 16:35:39 +0100

    Create TouiteRenderer.php

---
+++ src/classes/renderer/TouiteRenderer.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace touiteur\app\renderer;
+
+use touiteur\app\Exception\InvalidPropertyNameException;
+use touiteur\app\touite\Touite;
+
+class TouiteRenderer implements Renderer {
+    protected Touite $touite;
+
+    public function __construct(Touite $touite) {
+        $this->touite = $touite;
+    }
+
+    public function render(int $selector): string {
+        switch ($selector) {
+            case Renderer::LONG:
+                return $this->renderLong();
+            case Renderer::COMPACT:
+                return $this->renderCompact();
+            default:
+                return "Erreur : format de rendu inconnu";
+        }
+    }
+
+    protected function renderCompact(): string {
+        return "TODO";
+    }
+
+    protected function renderLong(): string {
+        return "TODO";
+    }
+
+    public function __get(string $at): mixed
+    {
+        if (!property_exists($this, $at)) {
+            throw new InvalidPropertyNameException(get_called_class() . "invalid property : $at");
+        }
+        return $this->$at;
+    }
+}
