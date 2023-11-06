2fef6ba416f256ae3c313a4865cb6f5a25ea9e1f
Maxence C <maxchaix54@gmail.com>
2023-11-06 16:35:30 +0100

    Update Touite.php

---
+++ src/classes/touite/Touite.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace touiteur\app\touite;
+
+use touiteur\app\Exception\InvalidPropertyNameException;
+
+class Touite {
+    private string $message;
+    private string $date;
+    private string $user;
+    private int $score;
+    private ?string $image = null;
+
+    public function __construct(string $message, string $user, ?string $image = null) {
+        $this->message = $message;
+        $this->date = date("Y-m-d H:i:s");
+        $this->user = $user;
+        $this->score = 0;
+        $this->image = $image;
+    }
+
+    public function __get($name): mixed {
+        if (!property_exists($this, $name)) {
+            throw new InvalidPropertyNameException("Invalid property name : $name");
+        }
+        return $this->$name;
+    }
+
+    public function addScore(int $score): void {
+        $this->score += $score;
+    }
+}
