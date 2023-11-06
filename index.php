991497baad7722504426bd2ed7d24e02db92b251
Z0ROZ <Fabioreveillard@gmail.com>
2023-11-06 15:49:23 +0100

    methode authentification et register

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 require_once "vendor/autoload.php";
 \touiteur\app\db\ConnectionFactory::setconfig("./conf.ini");
+\touiteur\app\db\ConnectionFactory::makeConnection();
---
+++ src/classes/Exception/AuthException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace touiteur\app\Exception;
+use Exception;
+class AuthException extends \Exception
+{
+
+}
