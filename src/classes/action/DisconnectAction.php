9bfeb761bbcea0a177683a2e04a5dcc24f957ae7
TheiLooK <louka@mellano.net>
2023-11-07 14:30:05 +0100

    Liens en couleur

--- src/classes/action/DisconnectAction.php
+++ src/classes/action/DisconnectAction.php
@@ -9,7 +9,7 @@ class DisconnectAction extends Action {
     public function execute(): string {
         session_destroy();
         $html = '<div><h3>Vous êtes déconnecté !</h3>';
-        $html .= '<a href="?action=display-home">Retour à l\'accueil</a></div>';
+        $html .= '<a class="lien" href="?action=display-home">Retour à l\'accueil</a></div>';
         return $html;
     }
 
