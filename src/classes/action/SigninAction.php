53aba41a574b8e628d28a55186c269da162f745a
TheiLooK <louka@mellano.net>
2023-11-07 10:40:03 +0100

    Front-end formulaires

--- src/classes/action/SigninAction.php
+++ src/classes/action/SigninAction.php
@@ -13,10 +13,10 @@ class SigninAction extends Action {
             }
             $pageContent = '
             <form method="POST" action="?action=signin">
-                <label for="email">Email :</label>
                 <input type ="text" id="email" placeholder="Email" name ="email">
-                <label for="pass">Mot de passe :</label>
+                <label for="email">Email</label>               
                 <input type ="password" id="pass" placeholder="Mot de passe" name ="pass">
+                <label for="pass">Mot de passe</label>
                 <input type="submit" value="Valider">
                 <a href="?action=register">Pas encore enregistré ? Créer un compte</a>
             </form>';
