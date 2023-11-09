931757181905e4de5d85ca8b56c20a1c3da0bb18
Maxence C <maxchaix54@gmail.com>
2023-11-09 11:50:28 +0100

    Page Profil

--- src/classes/action/AfficherProfil.php
+++ src/classes/action/AfficherProfil.php
@@ -3,8 +3,10 @@
 namespace touiteur\app\action;
 
 use touiteur\app\renderer\FeedRenderer;
+use touiteur\app\renderer\ListUserRenderer;
 use touiteur\app\renderer\Renderer;
 use touiteur\app\structure\lists\Feed;
+use touiteur\app\structure\lists\ListUser;
 use touiteur\app\structure\user\User;
 
 class AfficherProfil extends Action
@@ -16,6 +18,7 @@ class AfficherProfil extends Action
         } catch (\touiteur\app\Exception\InvalidUsernameException $e) {
             return "<h3>Utilisateur inconnu</h3>";
         }
+        $action = '?action=profil&user=' . $_GET['user'];
         try {
             $html = '<div class="profil">';
             $html .= '<div id="info"> <h3>' . $user->__get('prenom'). " " . $user->__get("nom") . '</h3>';
@@ -32,19 +35,65 @@ class AfficherProfil extends Action
                     $html .= '<input type="hidden" name="user" value="' . $user->__get("username") . '">';
                     $html .= '<input type="submit" value="Follow">';
                 }
+                $html .= '</form></div></div>';
+            }
+            else {
+                $content = $_POST['content'] ?? 'feed';
+                $html .= '</div><div class=home>';
+                if ($content === 'feed') {
+                    $html .= '<form method="POST" action="' . $action . '" class="active">';
+                } else {
+                    $html .= '<form method="POST" action="' . $action . '" class="inactive">';
+                }
+                $html .= '<input type="hidden" name="content" value="feed">';
+                $html .= '<input type="submit" value="Touites ">';
+                $html .= '</form>';
+
+                if ($content === 'follower') {
+                    $html .= '<form method="POST" action="' . $action . '" class="active">';
+                } else {
+                    $html .= '<form method="POST" action="' . $action . '" class="inactive">';
+                }
+                $html .= '<input type="hidden" name="content" value="follower">';
+                $html .= '<input type="submit" value="Followers">';
+                $html .= '</form>';
+
+                if ($content === 'following') {
+                    $html .= '<form method="POST" action="' . $action . '" class="active">';
+                } else {
+                    $html .= '<form method="POST" action="' . $action . '" class="inactive">';
+                }
+                $html .= '<input type="hidden" name="content" value="following">';
+                $html .= '<input type="submit" value="Following">';
                 $html .= '</form></div>';
             }
-            $html .= '</div>';
         } catch (\touiteur\app\Exception\InvalidPropertyNameException $e) {
             return "<h3>Utilisateur inconnu</h3>";
         }
 
-        $action = '?action=profil&user=' . $_GET['user'];
-        $feed = new Feed(Feed::LISTETOUITESPERSONNE, $action, $_GET['user'], null);
-        $feed->getListe($_GET['page'] ?? 1);
-        $r = new FeedRenderer($feed);
+        // récupérer POST content
+        $content = $_POST['content'] ?? 'feed';
+        switch ($content) {
+            case 'follower':
+                $listUser = new ListUser($user->email);
+                $listUser->getFollower();
+                $r = new ListUserRenderer($listUser);
+                $html .= $r->render(Renderer::COMPACT);
+                break;
+            case 'following':
+                $listUser = new ListUser($user->email);
+                $listUser->getFollowing();
+                $r = new ListUserRenderer($listUser);
+                $html .= $r->render(Renderer::COMPACT);
+                break;
+            case 'feed':
+                $feed = new Feed(Feed::LISTETOUITESPERSONNE, $action, $_GET['user'], null);
+                $feed->getListe($_GET['page'] ?? 1);
+                $r = new FeedRenderer($feed);
+                $html .= $r->render(Renderer::COMPACT);
+                break;
+        }
 
-        $html .= $r->render(Renderer::COMPACT);
         return $html;
     }
 }
--- src/classes/renderer/UserRenderer.php
+++ src/classes/renderer/UserRenderer.php
@@ -14,7 +14,7 @@ class UserRenderer implements Renderer {
 
     public function render(int $selector): string {
         $res = "<div>";
-        $res = '<h4><a href="?action=profil&user='.$this->user.'">'.$this->user.'</a></h4>';
+        $res = '<h4><a href="?action=profil&user='.$this->user->username.'">'.$this->user->username.'</a></h4>';
         return $res."</div>";
     }
 }
